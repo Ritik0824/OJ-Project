a51d2eda149c9c3afc909230aa50b60598d32d41
Ritik Yadav <ritikyadav4020@gmail.com>
2024-07-10 19:14:25 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,13 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors({
-    credentials: true,
-    origin: 'https://codeflowoj.netlify.app'  
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://codeflowoj.netlify.app'); // Replace with your frontend's origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    next();
+  });
 
 // Initialize database
 DBConnection();
