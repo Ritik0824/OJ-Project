8ba94dd58eb61488db007bd735ae83d431dfb3ec
Ritik <121361030+Ritik0824@users.noreply.github.com>
2024-08-27 10:35:53 +0530

    Update index.js
    Resolved server error

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: 'https://codeflowoj.netlify.app', // your frontend domain
+    origin: 'https://codeflowoj2.netlify.app', // your frontend domain
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // if you are using cookies
